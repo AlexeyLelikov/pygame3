37f62ad60ac662c6d48b7c4d2006d4aba4bcbab9
AlexeyLelikov <alexeylelikove@gmail.com>
2022-05-08 16:26:49 +0300

    clas  FireBall

--- FireBall/class_of_sprite.py
+++ FireBall/class_of_sprite.py
@@ -7,4 +7,26 @@ class Sprite(pygame.sprite.Sprite): # Класс родитель
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect(topleft = (x,y))
 class Player(Sprite):
-    pass
+    def __init__(self,img,x,y,speedx,animR,animL):
+        Sprite.__init__(self,img,x,y)
+        self.speedx = speedx
+        self.AnimGoL = animL
+        self.AnimGoR = animR
+        self.cadr = 0
+        self.game = True
+    def update(self,key,GroupFireBall):
+        if key[pygame.K_LEFT]:
+            self.x -= self.speed
+            self.image = imgPlayerGoL[self.cadr % 5]
+        if key[pygame.K_RIGHT]:
+            self.x += self.speed
+            self.image = imgPlayerGoR[self.cadr % 5]
+        if collideG(self.rect,GroupFireBall):
+            self.game = False
+        self.cadr += 1
+
+
+
+
+
+
---
+++ FireBall/lib_collide.py
@@ -0,0 +1,22 @@
+
+
+def collide(Sprite1,Sprite2):
+    if ((     Sprite1.x <=  Sprite2.x  <= Sprite1.x + Sprite1.width
+        and   Sprite1.y <= Sprite2.y <= Sprite1.y + Sprite1.height)
+        or   (Sprite1.x <= Sprite2.x + Sprite2.width  <= Sprite1.x + Sprite1.width
+        and   Sprite1.y <= Sprite2.y + Sprite2.height <= Sprite1.y + Sprite1.height)
+        or   (Sprite2.x <= Sprite1.x + Sprite1.width  <= Sprite2.x + Sprite2.width
+        and   Sprite2.y <= Sprite1.y <= Sprite2.y + Sprite2.height)
+        or   (Sprite2.x <= Sprite1.x <= Sprite2.x + Sprite2.width
+        and   Sprite2.y <= Sprite1.y + Sprite1.height <= Sprite2.y + Sprite2.height)):
+        return  True
+    else:
+        return False
+
+
+def collideG(Sprite,Group):
+    IsCollide = False
+    for Spr in Group.sprites():
+        if collide(Sprite.rect,Spr.rect):
+            IsCollide = True
+    return IsCollide
