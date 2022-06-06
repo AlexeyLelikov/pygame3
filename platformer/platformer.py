68da0b0ece3a115e9a1088c960ae52f9dcc572d4
AlexeyLelikov <alexeylelikove@gmail.com>
2022-06-06 15:53:37 +0300

    new FireBall

--- platformer/platformer.py
+++ platformer/platformer.py
@@ -15,5 +15,28 @@ class Camera:
         if player.rect.y < WorldHeight - ScreenHeight / 2 and player.rect.y > ScreenHeight / 2:
             self.y = int(ScreenHeight / 2) - player.rect.y
 
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self,x, y, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect(topleft = (x, y))
+
+class Player(Sprite):
+    def __init__(self, x, y, img, speed):
+        super().__init__(x, y, img)
+        self.speed = speed
+        self.cadr = 0
+    def update(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_RIGHT]:
+            self.rect.x += speed
+        elif key[pygame.K_LEFT]:
+            self.rect.x -= speed
+        elif key[pygame.K_UP]:
+            self.rect.y -= speed
+        elif key[pygame.K_DOWN]:
+            self.rect.y += speed
+
+
 
 
